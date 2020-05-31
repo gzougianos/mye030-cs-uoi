0dfd60ff20b183acc98391b2b1b975c243192c65
gzougianos <g.zougianos@gmail.com>
2020-05-31 19:24:39 +0300

    fix for problematic lines in csv file parser

--- database_related/country_parser/src/main/java/gr/uoi/cs/mye30/parser/CsvFileParser.java
+++ database_related/country_parser/src/main/java/gr/uoi/cs/mye30/parser/CsvFileParser.java
@@ -3,6 +3,7 @@ package gr.uoi.cs.mye30.parser;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -20,12 +21,35 @@ public class CsvFileParser {
 		List<String> allLines = Files.readAllLines(csvFile.toPath());
 		IntStream.range(0, recordParser.numberOfLinesToSkip()).forEach(i -> allLines.remove(0));
 
+		removeProblematicLinesAndReAddThemNormally(allLines);
+
 		//@formatter:off
 		return allLines.stream()
 				.map(CsvFileParser::split)
 				.map(recordParser::create)
 				.collect(Collectors.toList());
 		//@formatter:on
+	}
+
+	private static void removeProblematicLinesAndReAddThemNormally(List<String> allLines) {
+		List<String> problematicLines = allLines.stream().filter(l -> !l.startsWith("\"") || !l.endsWith("\","))
+				.collect(Collectors.toList());
+
+		allLines.removeAll(problematicLines);
+
+		StringBuilder sb = new StringBuilder();
+		problematicLines.stream().filter(l -> !l.isEmpty()).forEach(l -> {
+			if (l.startsWith("\"")) {
+				sb.append(System.lineSeparator());
+				sb.append(l);
+			} else {
+				sb.append(l);
+			}
+		});
+
+		allLines.addAll(Arrays.asList(sb.toString().trim().split(System.lineSeparator())));
+
+		allLines.removeIf(String::isEmpty);
 	}
 
 	private static String[] split(String line) {
